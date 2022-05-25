d5375734fec91466c7f19695f09ae6f8715c64d3
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-05-25 18:00:39 +0200

    Update constants

--- constants.py
+++ constants.py
@@ -3,4 +3,6 @@ SONAR_PATH = "data/sonar/"
 DUTCHSEMCOR_DIR_PATH = "data/dutsemcor/"
 DUTCHSEMCOR_FILTERED_CSV_PATH = "data/dutsemcor_filtered/"
 SENSE_EX = "sense_examples/"
+SENSE_EX_VEC = "sense_examples_with_vectors/"
 NO_SAMPLES = 60
+VECTORS = "vectors/"
