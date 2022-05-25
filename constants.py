ed4cdf3a659f19152da9b6f06cc0f0deea4bc2a8
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-05-25 16:28:52 +0200

    Define constants file

---
+++ constants.py
@@ -0,0 +1,6 @@
+DATA_PATH = "data/"
+SONAR_PATH = "data/sonar/"
+DUTCHSEMCOR_DIR_PATH = "data/dutsemcor/"
+DUTCHSEMCOR_FILTERED_CSV_PATH = "data/dutsemcor_filtered/"
+SENSE_EX = "sense_examples/"
+NO_SAMPLES = 30
